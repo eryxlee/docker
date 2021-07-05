5c096018437a1b5cacdbc4441d9d5a7e4936c573
Eryx Lee <eryx.lee0901@gmail.com>
2021-07-05 15:57:03 +0800

    init command of centos

--- bin/sys-init.sh
+++ bin/sys-init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# 添加更新源国内开源镜像的地址。腾讯ECS中已经处理
+#
+# 更新系统
+#yum -y update && yum -y upgrade
 # 关闭防火墙
 sudo systemctl stop firewalld && systemctl disable firewalld
 # 重置iptables
@@ -8,5 +12,8 @@ sudo swapoff -a
 sudo sed -i '/swap/s/^\(.*\)$/#\1/g' /etc/fstab
 # 关闭selinux
 sudo setenforce 0
+# 永久关闭 修改/etc/sysconfig/selinux文件设置
+sed -i 's/SELINUX=permissive/SELINUX=disabled/' /etc/sysconfig/selinux
+sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
 # 关闭dnsmasq(否则可能导致docker容器无法解析域名)
 sudo systemctl stop dnsmasq && systemctl disable dnsmasq
