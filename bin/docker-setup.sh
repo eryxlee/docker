4f8f12d090bf085a533956fdf6b27b8c428b5947
Eryx Lee <eryx.lee0901@gmail.com>
2021-07-05 16:12:58 +0800

    change repo to aliyun

--- bin/docker-setup.sh
+++ bin/docker-setup.sh
@@ -11,7 +11,7 @@ sudo yum install -y yum-utils device-mapper-persistent-data  lvm2
 
 sudo yum-config-manager \
     --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+    http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 
